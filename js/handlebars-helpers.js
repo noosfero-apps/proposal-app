e4c7476f218998424e18fe321c706c7ecf2aa0d3
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-04-14 09:17:27 -0300

    creating a list_proposal helpr

--- js/handlebars-helpers.js
+++ js/handlebars-helpers.js
@@ -6,3 +6,24 @@ Handlebars.registerHelper('link', function(text, url) {
 
   return new Handlebars.SafeString(result);
 });
+
+Handlebars.registerHelper('list_proposal', function(proposals, options) {
+
+  var ret = "";
+
+  for(var i=0, j=proposals.length; i<j; i++) {
+    ret = ret + "<li class='proposal-item'>";
+    ret = ret + "<ul class='category'>";
+     
+    for(var x=0, y=proposals[i].categories.length; x<y; x++) {
+      ret = ret + '<li>' + proposals[i].categories[x].name + '</li>';
+    }
+    ret =  ret + '</ul>';
+    ret = ret + options.fn(proposals[i]);
+    ret = ret + '<p>' + proposals[i].body + '</p>';
+    ret = ret + '</li>';
+  }
+
+  return ret;
+
+});
