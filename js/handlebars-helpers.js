f42e60cff3597442bdd3da3f159bd07ac7753834
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-04-19 16:20:04 -0300

    change category list

--- js/handlebars-helpers.js
+++ js/handlebars-helpers.js
@@ -12,21 +12,23 @@ Handlebars.registerHelper('list_proposal', function(proposals, options) {
   var ret = "";
   for(var i=0, j=proposals.length; i<j; i++) {
     element = "<li class='proposal-item'><div class='item'>";
-    element = element + "<ul class='category'>";
+    category = "<ul class='category'>";
      
     for(var x=0, y=proposals[i].categories.length; x<y; x++) {
       if((options.hash['category'] != null) && (options.hash['category'] != proposals[i].categories[x].slug)){
         element = '';
         continue;
       }      
-      element = element + '<li class="category-'+proposals[i].categories[x].slug+'">' + proposals[i].categories[x].name + '</li>';
+      category = category + '<li class="category-'+proposals[i].categories[x].slug+'">' + proposals[i].categories[x].name + '</li>';
     }
     if(element == ''){
       continue;
     }
-    element =  element + '</ul>';
+    category =  category + '</ul>';
     element = element + options.fn(proposals[i]);
-    element = element + '<p>' + (proposals[i].abstract ? proposals[i].abstract : '') + '</p>';
+    element = element + (proposals[i].abstract ? proposals[i].abstract : '');
+
+    element = element + category;
     ret = ret + element + '</div></li>';
   }
   return ret;
