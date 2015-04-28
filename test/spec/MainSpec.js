3d10c1fb0bfb3bb7469bb5ddc0ae32c9f6ea7d19
Evandro Junior <evandrojr@gmail.com>
2015-04-28 14:59:42 -0300

    Changes to support automated testing

--- test/spec/MainSpec.js
+++ test/spec/MainSpec.js
@@ -1,5 +1,8 @@
-describe("addVotedProposal", function(){
-  it("Should register a voted id", function(){
-      addVotedProposal(1).toEqual(true);
+define(["model/main"],function(){
+
+  describe("addVotedProposal", function(){
+    it("Should register a voted id", function(){
+        addVotedProposal(1).toEqual(true);
+    });
   });
 });
