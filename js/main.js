a501bda909718ed790db7ef07b04de8963b9af8f
Victor Costa <vfcosta@gmail.com>
2015-04-29 16:00:54 -0300

    Change post url for new proposals depending on moderate_proposals setting

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ define(['handlebars'], function(Handlebars){
   }
 
   // Load data from localhost when it is dev env.
-  var noosferoAPI = host + '/api/v1/articles/' + proposal_discussion + '?private_token=' + private_token + '&fields=id,children,categories,abstract,title,image,url';
+  var noosferoAPI = host + '/api/v1/articles/' + proposal_discussion + '?private_token=' + private_token + '&fields=id,children,categories,abstract,title,image,url,setting';
 
   $.getJSON(noosferoAPI)
     .done(function( data ) {
