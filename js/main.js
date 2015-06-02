723e491c6b7baab6b31cd532822a117d746cb6de
augustoalmeida <augusto.almeida@gmail.com>
2015-06-01 21:19:22 -0300

    Remoção de hide

--- js/main.js
+++ js/main.js
@@ -224,19 +224,19 @@ define(['handlebars', 'fastclick', 'handlebars_helpers'], function(Handlebars, F
           return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
         },
         display_category_tab: function(){
-          $('#proposal-group').hide();
+          // $('#proposal-group').hide();
           $('#proposal-categories').show();
           $('#nav-proposal-categories a').addClass('active');
           $('#nav-proposal-group a').removeClass('active');
           $('.proposal-category-items').hide();
-          $('.proposal-category .arrow-box').hide();
+          // $('.proposal-category .arrow-box').hide();
           $('.proposal-detail').hide();
 
           $('#content').show();
           $('nav').show();
         },
         display_proposals_tab: function(){
-          $('#proposal-categories').hide();
+          // $('#proposal-categories').hide();
           $('#proposal-group').show();
           $('#nav-proposal-group a').addClass('active');
           $('#nav-proposal-categories a').removeClass('active');
@@ -329,9 +329,9 @@ define(['handlebars', 'fastclick', 'handlebars_helpers'], function(Handlebars, F
                 $('html, body').animate({ scrollTop: itemOffset.top }, 'fast');
               }
             } );
-            $('.proposal-category .arrow-box').hide();
+            // $('.proposal-category .arrow-box').hide();
             var categorySlug = $item.data('category');
-            $('#proposal-category-' + categorySlug).find('.arrow-box').show();
+            // $('#proposal-category-' + categorySlug).find('.arrow-box').show();
 
           }
         },
