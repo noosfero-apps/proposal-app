635fcd621aa640c8fed626fcdb0fd558ee4ffdbb
Leonardo Merlin <leonardo.merlin@serpro.gov.br>
2015-06-22 07:27:20 -0300

    Deprecated code?

--- js/main.js
+++ js/main.js
@@ -1143,29 +1143,29 @@ define(['jquery', 'handlebars', 'fastclick', 'handlebars_helpers', 'piwik'], fun
       e.preventDefault();
     });
 
-    var popupCenter = function(url, title, w, h) {
-      var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
-      var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+    // var popupCenter = function(url, title, w, h) {
+    //   var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+    //   var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
 
-      var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-      var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+    //   var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    //   var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 
-      var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-      var top = ((height / 3) - (h / 3)) + dualScreenTop;
+    //   var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+    //   var top = ((height / 3) - (h / 3)) + dualScreenTop;
 
-      var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    //   var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
 
-      // Puts focus on the newWindow
-      if (window.focus) {
-        newWindow.focus();
-      }
-    };
+    //   // Puts focus on the newWindow
+    //   if (window.focus) {
+    //     newWindow.focus();
+    //   }
+    // };
 
-    $(document).on('click', '.social a.popup', {}, function popUp(e) {
-      var self = $(this);
-      popupCenter(self.attr('href'), self.find('.rrssb-text').html(), 580, 470);
-      e.preventDefault();
-    });
+    // $(document).on('click', '.social a.popup', {}, function popUp(e) {
+    //   var self = $(this);
+    //   popupCenter(self.attr('href'), self.find('.rrssb-text').html(), 580, 470);
+    //   e.preventDefault();
+    // });
 
     $(document).on('click', '#logout-button', function (e){
       var self = $(this);
