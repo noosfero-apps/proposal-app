2ef405e3ebc3cbab52dec8bedd9f14f63a1fc77b
Victor Costa <vfcosta@gmail.com>
2015-09-03 10:02:53 -0300

    Piwik migration

--- js/piwik.js
+++ js/piwik.js
@@ -7,7 +7,7 @@ define(['piwik'], function(){
   (function() {
     var u="http://estatisticas.dialoga.gov.br/";
     _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "1"]);
+    _paq.push(["setSiteId", "65"]);
     var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
     g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
   })();
